a901f93710fa917baf80439a1320484f77e8beb3
krononava <35059807+krononava@users.noreply.github.com>
2026-07-12 15:01:30 +0800

    initial commit

---
+++ extension.js
@@ -0,0 +1,9 @@
+const responseBlock = document.querySelector('[data-element-id="response-block"]');
+if (responseBlock) {
+  responseBlock.style.paddingLeft = '0.5rem';
+}
+
+const avatarContainer = document.querySelector('[data-element-id="chat-avatar-container"]');
+if (avatarContainer) {
+  avatarContainer.style.display = 'none';
+}
